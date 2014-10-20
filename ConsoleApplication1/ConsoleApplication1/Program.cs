ccbdc436d035a81deef769271d87f3c1855d521e
Edgar Manuel García González <manolo161070@hotmail.com>
2014-10-20 08:24:40 -0500

    ...

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -12,26 +12,26 @@ namespace ConsoleApplication1
         {
             string negra = "####    ";
             string blanca = "    ####";
-            string negra2 = "####    \n";
-            string blanca2 = "    ####\n";
 
-            for (int i = 0; i <= 2; i++) 
+            for (int i = 0; i <= 3; i++) 
             {
 
                 for (int x = 1; x <= 4; x++) 
                 {
-                    Console.Write(negra);
-                    Console.Write(negra);
-                    Console.Write(negra);
-                    Console.Write(negra2);
+                    for (int y = 1; y <= 3; y++)
+                    {
+                        Console.Write(negra);
+                    }
+                    Console.Write(negra + "\n");
                 }
 
                 for (int y = 1; y <= 4; y++) 
                 {
-                    Console.Write(blanca);
-                    Console.Write(blanca);
-                    Console.Write(blanca);
-                    Console.Write(blanca2);
+                    for (int z = 1; z <= 3; z++) 
+                    {
+                        Console.Write(blanca);
+                    }
+                    Console.Write(blanca + "\n");
                 }
        
             }
